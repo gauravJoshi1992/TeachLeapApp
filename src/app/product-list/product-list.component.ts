46de48bbe48cac274f1fc095539e3f39c6df2d50
mahashweta1 <31702295+mahashweta1@users.noreply.github.com>
2021-12-24 01:13:42 +0530

    UI improved

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -10,9 +10,29 @@ import {NgbRatingConfig} from '@ng-bootstrap/ng-bootstrap';
 })
 export class ProductListComponent implements OnInit {
  public  productList: any;
+ public height: any = '10rem';
+ public width: any = '10rem';
+ public filter: any = {
+   "gender": [
+    {"title":"Men"},
+    {"title":"Women"},
+    {"title":"Boys"},
+    {"title":"Girls"},
+   ],
+   "Category": [
+     {"title": "men's clothing"},
+     {"title": "jewelery"},
+     {"title": "electronics"},
+     {"title": "women's clothing"}
+   ]
+ }
+  genderData: any;
+  categoryData: any;
   constructor(private productListService: ProductListService) { }
 
   ngOnInit() {
+    this.genderData = this.filter.gender;
+    this.categoryData = this.filter.Category
     this.getProductList();
   }
 
